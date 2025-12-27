7b56128e9d3e8044b5eb24211556f0ddb90b6c4c
Ella S <soccoli2015@gmail.com>
2025-12-27 14:24:59 -0500

    Added page routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import './App.css'
 import Navbar from './Navbar'
 import BingoCard from './BingoCard'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import ChallengeCard from './ChallengeCard'
 
 function App() {
   return (
     <>
-      <Navbar />
-      <div>
-          <h1 className='text-4xl font-bold text-center my-5'>Welcome to the Couriway No Reset Bingo!</h1>
-      </div>
-      <BingoCard />
+      <BrowserRouter>
+          <Navbar />
+            <div>
+              <h1 className='text-4xl font-bold text-center my-5'>Welcome to the Couriway No Reset Bingo!</h1>
+            </div>
+
+          <Routes>
+            <Route path="/" element={<BingoCard />} />
+            <Route path="/100k" element={<ChallengeCard />} />
+      </Routes>
+      </BrowserRouter>
+
     </>
   )
 }
--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,11 +1,12 @@
+import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
         <>
             <nav className="bg-purple-800 w-full text-2xl">
-                <div className="px-5 py-3 inline-block align-middle hover:bg-purple-900"><a href="/"><img src="/CouriwayLogo.svg" width={50} /></a></div>
-                <div className="px-5 py-5 inline-block text-yellow-300 align-middle hover:bg-purple-900"><a href="/bingo">Bingo</a></div>
-                <div className="px-5 py-5 inline-block text-yellow-300 align-middle hover:bg-purple-900"><a href="/100k">100k Bingo</a></div>
+                <Link to={'/'} className="px-5 py-3 inline-block align-middle hover:bg-purple-900"><a href="/"><img src="/CouriwayLogo.svg" width={50} /></a></Link>
+                <Link to={'/'} className="px-5 py-5 inline-block text-yellow-300 align-middle hover:bg-purple-900">Bingo</Link>
+                <Link to={'/100k'} className="px-5 py-5 inline-block text-yellow-300 align-middle hover:bg-purple-900">100k Bingo</Link>
             </nav>
         </>
     )
